9f118df639d1c08839cebcc99d5bd2f997f8ee85
alexg <alex.galvis.sistemas@gmail.com>
2023-10-24 14:57:59 -0500

    Add cluster capabilities to handle more requests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { promises } from 'fs';
 import helmet from 'helmet';
 import { join } from 'path';
 import { AppModule } from './module/app.module';
+import { ClusterService } from './service/cluster.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -31,4 +32,4 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   await app.listen(configService.get('server.port'));
 }
-bootstrap();
+ClusterService.clusterize(bootstrap);
